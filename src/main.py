abbee56afebcf093d2ddbd0b3e514ae5b2b4d104
Henok Teixeira <henokteixeira@gmail.com>
2025-02-28 21:22:53 -0300

    feat: add main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from sqlmodel import SQLModel
+
+from .database.infra import engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
