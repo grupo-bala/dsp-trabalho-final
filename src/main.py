1a5e598e484b2fe14b3972dc95c7da88ab7b1343
Matheus Leandro <matheusleandro33@gmail.com>
2025-03-06 21:54:17 -0300

    feat: add routes to unidadegestora model

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from sqlmodel import SQLModel
 from src.routes.programa import router as programa_router
 from src.routes.transferencia import router as transferencia_router
+from src.routes.unidade_gestora import router as unidade_gestora
 
 from .database.infra import engine
 
@@ -18,6 +19,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(programa_router)
 app.include_router(transferencia_router)
+app.include_router(unidade_gestora)
 
 @app.get("/")
 async def read_root():
---
+++ src/routes/unidade_gestora.py
@@ -0,0 +1,71 @@
+from typing import List, Optional
+from fastapi import APIRouter, HTTPException, Depends, Query
+from sqlmodel import Session, select
+from src.models import UnidadeGestora
+from src.database.infra import get_session
+
+router = APIRouter(prefix="/unidades_gestoras", tags=["Unidades Gestora"])
+
+@router.post("/unidades_gestoras/", response_model=UnidadeGestora)
+def create_unidade_gestora(unidade_gestora: UnidadeGestora, session: Session = Depends(get_session)):
+    try:
+        session.add(unidade_gestora)
+        session.commit()
+        session.refresh(unidade_gestora)
+        return unidade_gestora
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail=f"Erro ao criar transferencia: {str(e)}")
+
+@router.get("/unidades_gestoras/", response_model=List[UnidadeGestora])
+def read_transferencia(
+    session: Session = Depends(get_session),
+    skip: int = Query(0, alias="offset", ge=0),
+    limit: int = Query(10, le=100),
+    orgao_nome: Optional[str] = Query(None, alias="orgao_nome")
+):
+    try:
+        query = select(UnidadeGestora)
+        if orgao_nome:
+            query = query.where(UnidadeGestora.orgao_nome.contains(orgao_nome))
+        transferencia = session.exec(query.offset(skip).limit(limit)).all()
+        return transferencia
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar transferencia: {str(e)}")
+
+@router.get("/unidades_gestoras/{codigo}", response_model=UnidadeGestora)
+def read_transferencia(codigo: int, session: Session = Depends(get_session)):
+    unidadeGestora = session.get(UnidadeGestora, codigo)
+    if not unidadeGestora:
+        raise HTTPException(status_code=404, detail="Transferencia não encontrado")
+    return unidadeGestora
+
+@router.put("/unidades_gestoras/{codigo}", response_model=UnidadeGestora)
+def update_transferencia(codigo: int, transferencia_update: UnidadeGestora, session: Session = Depends(get_session)):
+    unidadeGestora = session.get(UnidadeGestora, codigo)
+    if not unidadeGestora:
+        raise HTTPException(status_code=404, detail="Transferencia não encontrada")
+    try:
+        update_data = transferencia_update.dict(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(unidadeGestora, key, value)
+        session.add(unidadeGestora)
+        session.commit()
+        session.refresh(unidadeGestora)
+        return unidadeGestora
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail=f"Erro ao atualizar unidadeGestora: {str(e)}")
+
+@router.delete("/unidades_gestoras/{codigo}", response_model=UnidadeGestora)
+def delete_transferencia(codigo: int, session: Session = Depends(get_session)):
+    unidadeGestora = session.get(UnidadeGestora, codigo)
+    if not unidadeGestora:
+        raise HTTPException(status_code=404, detail="Unidade gestora não encontrada")
+    try:
+        session.delete(unidadeGestora)
+        session.commit()
+        return unidadeGestora
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail=f"Erro ao deletar transferencia: {str(e)}")
