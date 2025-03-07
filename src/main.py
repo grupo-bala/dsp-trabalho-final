5a0b69d1dc9e5dd8de4d2b8d42b27e8603fc6152
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2025-03-07 17:27:01 -0300

    feat: add logs

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
+import sys
 from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from sqlmodel import SQLModel
 from src.routes.programa import router as programa_router
 from src.routes.transferencia import router as transferencia_router
 from src.routes.unidade_gestora import router as unidade_gestora
-
+from loguru import logger
 from .database.infra import engine
+
+LOGGER_FORMAT = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+    "<level>{level: <8}</level> | "
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+    "<level>{message} - Additional information: {extra}</level>"
+)
+
+logger.remove()
+logger.add(sys.stderr, format=LOGGER_FORMAT)
+logger.add("actions.log")
 
 
 @asynccontextmanager
@@ -16,6 +27,24 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    inner_log = logger
+    inner_log.info(f"Receiving request: {request.method} {request.url}")
+
+    response = None
+
+    try:
+        response = await call_next(request)
+        inner_log.info(f"Response: {response.status_code}")
+    except Exception as error:
+        inner_log.error(f"Response: 500")
+        raise error
+
+    return response
+
 
 app.include_router(programa_router)
 app.include_router(transferencia_router)
